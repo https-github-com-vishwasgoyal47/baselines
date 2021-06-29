3e2f929ad75a015bfe77f63114f6e8cf060d077d
Vishwas G <vishwasg.intern@PP-C02FD0JBMD6M.local>
2021-06-29 11:30:42 +0530

    added Data class

--- baseline/data.py
+++ baseline/data.py
@@ -8,3 +8,16 @@ def convert(my_name):
     """
 
     print(f"I'll convert a notebook for you some day, {my_name}.")
+
+class Data():
+    def __init__(self) -> None:
+        pass
+    def add_dataset(dataset):
+        print(dataset)
+    def show_dataset():
+        import seaborn
+        return seaborn.get_dataset_names()
+    def load_dataset(name):
+        import seaborn
+        dataset = seaborn.load_dataset(name)
+        return dataset
