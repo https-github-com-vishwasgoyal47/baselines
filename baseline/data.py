a55b0ed887338cce67d9ab3fba5d83bf97e67178
Vishwas G <vishwasg.intern@PP-C02FD0JBMD6M.local>
2021-06-29 23:13:17 +0530

    added classification class for Randomforest and Support Vector

--- baseline/data.py
+++ baseline/data.py
@@ -12,12 +12,12 @@ def convert(my_name):
 class Data():
     def __init__(self) -> None:
         pass
-    def add_dataset(dataset):
+    def add_dataset(self,dataset):
         print(dataset)
-    def show_dataset():
+    def show_dataset(self):
         import seaborn
         return seaborn.get_dataset_names()
-    def load_dataset(name):
+    def load_dataset(self,name):
         import seaborn
         dataset = seaborn.load_dataset(name)
         return dataset
